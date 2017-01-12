c1a01cdcf537f452e4356ac254d4819cdd7c50c8
Eugene Vendensky <eugenevendensky@zipcoders-MacBook-Pro-3.local>
2017-01-12 11:48:50 -0500

    Base written

---
+++ src/main/java/vedensky/eugene/currencyconverter/CurrencyConverter.java
@@ -0,0 +1,67 @@
+package vedensky.eugene.currencyconverter;
+
+/**
+ * Created by eugenevendensky on 1/12/17.
+ */
+public class CurrencyConverter {
+
+        private Double dollar;
+        private Double euro;
+        private Double britishPound;
+        private Double indianRupee;
+        private Double australianDollar;
+        private Double canadianDollar;
+        private Double singaporeDollar;
+        private Double swissFranc;
+        private Double malaysianRingit;
+        private Double japaneseYen;
+        private Double yuanRenminbi;
+
+
+        public CurrencyExchanger(){
+            this.dollar = 1.00
+            this.euro = .94;
+            this.britishPound =.82;
+            this.indianRupee = 68.32;
+            this.australianDollar = 1.35;
+            this.canadianDollar = 1.32;
+            this.singaporeDollar = 1.43;
+            this.swissFranc =  1.01;
+            this.malaysianRingit = 4.47;
+            this.japaneseYen = 115.84;
+            this.yuanRenminbi = 6.92;
+        }
+        public Double currencyCalculator(Double from, Double to ) {
+            return (dollar / from) * to;
+        }
+        public Double euroToDollar(){
+            return dollar/euro;
+        }
+        public Double euroToBritishPound(){
+            return (dollar/euro) * britishPound;
+        }
+
+        public Double britishPoundToIndianRupee(){
+            return (dollar/britishPound) * indianRupee;
+        }
+        public Double rupeeToCanadianDollar(){
+            return (dollar/indianRupee) * indianRupee;
+        }
+        public Double canadianDollarToSingaporeDollar(){
+            return (dollar/canadianDollar) * singaporeDollar;
+        }
+        public Double singaporeDollarToSwissFranc(){
+            return (dollar/britishPound) * indianRupee;
+        }
+        public Double swissFrancToMalaysianRinggit(){
+            return (dollar/swissFranc)* malaysianRingit;
+        }
+        public Double japaneseYenToChineseYuanRenminbi(){
+            return (dollar/japaneseYen) * yuanRenminbi;
+        }
+        public Double malaysianRingitToJapaneseYen(){
+            return (dollar/malaysianRingit) * japaneseYen;
+        }
+    }
+
+}
---
+++ src/test/java/vedensky/eugene/currencyconverter/CurrencyConverterTest.java
@@ -0,0 +1,24 @@
+package vedensky.eugene.currencyconverter; /**
+ * Created by eugenevendensky on 1/12/17.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import vedensky.eugene.currencyconverter.CurrencyConverter;
+
+public class CurrencyConverterTest {
+
+    CurrencyConverter currencyConverter;
+
+    @Before
+    public void setUp() {
+        currencyExchanger = new CurrencyExchanger();
+    }
+    @Test
+    public void (){
+        Double expected = 0.7707999999999999;
+        Double actual = currencyExchanger.eurotoBritishPound();
+        assertEquals("I expect that the turnt level will be .7708",expected,actual);
+}
