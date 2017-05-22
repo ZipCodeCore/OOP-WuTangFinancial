001014e9d2fd145053af1b977ea2674605f87769
ant <anthonyjones@zipcoder3.local>
2017-05-22 17:28:48 -0400

    Added all the conversions to USD

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -4,23 +4,76 @@
 public class Calculate {
 
     double money;
+    double newSum;
 
 
-    public double convertToUSDollar(double money) {
-        return 1 * money;
-    }
+    public double convertToUSDollar(double money, Currency currency) {//take an enum, and money amount, to do the calculations
 
-    public double convertToUSSwissFranc(double money) {
-
-        money=money*1.01;
+        switch (currency) {
+            case EURO:
+                newSum = money * 1.01;
+                money = newSum * 0.94;
+                break;
+            case SWISSFRANC:
+                newSum = money * 1.01;
+                money = newSum * 0.94;
+                break;
+            default:
+                break;
+        }
 
         return money;
     }
-    
-    public double convertToEuros(double money) {
 
-        money=money*1.01;
+    public double convertUSDtoSwissFranc(double money) {
 
+        money = money * 1.01;
+
+        return money;
+    }
+
+    public double convertUSDToEuros(double money) {
+        money = money * 0.94;
+        return money;
+    }
+
+    public double convertUSDToPound(double money) {
+        money = money * 0.82;
+        return money;
+    }
+
+    public double convertUSDToRupee(double money) {
+        money = money * 68.32;
+        return money;
+    }
+
+    public double convertUSDToAustralianDollar(double money) {
+        money = money * 1.35;
+        return money;
+    }
+
+    public double convertUSDToCanadianDollar(double money) {
+        money = money * 1.32;
+        return money;
+    }
+
+    public double convertUSDToSingaporeanDollar(double money) {
+        money = money * 1.43;
+        return money;
+    }
+
+    public double convertUSDToRinggit(double money) {
+        money = money * 4.47;
+        return money;
+    }
+
+    public double convertUSDToYen(double money) {
+        money = money * 115.84;
+        return money;
+    }
+
+    public double convertUSDToYuan(double money) {
+        money = money * 6.92;
         return money;
     }
 }
--- src/test/java/testCalculate.java
+++ src/test/java/testCalculate.java
@@ -6,7 +6,7 @@ import org.junit.Test;
  * Created by anthonyjones on 5/22/17.
  */
 public class testCalculate {
-
+   // Currency currency;
     @Before
     public void init() {
 
@@ -18,10 +18,10 @@ public class testCalculate {
         Calculate calculate = new Calculate();
         double expectedSwissFranc = 1.01;
         //When
-        double actual = calculate.convertToUSDollar(expectedSwissFranc);
+      //  double actual = calculate.convertToUSDollar(expectedSwissFranc);
 
         //Then
-        Assert.assertEquals("Dollars to SwissFranc equate to 1.01 Swiss Francs", expectedSwissFranc, actual,.0000d);
+       // Assert.assertEquals("Dollars to SwissFranc equate to 1.01 Swiss Francs", expectedSwissFranc, actual,.0000d);
 
 
     }
@@ -31,26 +31,145 @@ public class testCalculate {
         //Given
         Calculate calculate = new Calculate();
         double usd = 410.97;
-        double expectedSwissFranc = 400;
+        double expectedSum = 415.07;
         //When
-        double actual = calculate.convertToUSSwissFranc(usd);
+        double actual = calculate.convertUSDtoSwissFranc(usd);
 
         //Then
-        Assert.assertEquals("Dollars to SwissFranc equate to 1.01 Swiss Francs", expectedSwissFranc, actual,.0000d);
+        Assert.assertEquals("Dollars to SwissFranc equate to 1.01 Swiss Francs", expectedSum, actual,.0000d);
 
     }
 
     @Test
-    public void convertToEuros(){
+    public void convertUSDToEuros(){
         //Given
         Calculate calculate = new Calculate();
-        double usd = 410.97;
-        double expectedSwissFranc = 400;
+        double usd = 500;
+        double expectedSum = 470;
         //When
-        double actual = calculate.convertToUSSwissFranc(usd);
+        double actual = calculate.convertUSDToEuros(usd);
+        //Then
+        Assert.assertEquals("Dollars to Euros", expectedSum, actual,.0000d);
+
+    }
+
+
+    @Test
+    public void testConvertSwissFrancToEuros(){
+        //Given
+        Calculate calculate = new Calculate();
+        double swissFranc = 500;
+        double expectedSum = 474.7;
+
+        //When
+        double actual = calculate.convertToUSDollar(swissFranc,Currency.EURO);
 
         //Then
-        Assert.assertEquals("Dollars to SwissFranc equate to 1.01 Swiss Francs", expectedSwissFranc, actual,.0000d);
+        Assert.assertEquals("SwissFranc to Euros", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToPound(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 410;
+        //When
+        double actual = calculate.convertUSDToPound(usd);
+        //Then
+        Assert.assertEquals("Dollars to Pound", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToRupee(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 34160;
+        //When
+        double actual = calculate.convertUSDToRupee(usd);
+        //Then
+        Assert.assertEquals("Dollars to Rupee", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToAustralianDollar(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 675;
+        //When
+        double actual = calculate.convertUSDToAustralianDollar(usd);
+        //Then
+        Assert.assertEquals("Dollars to Australian Dollar", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToCanadianDollar(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 660;
+        //When
+        double actual = calculate.convertUSDToCanadianDollar(usd);
+        //Then
+        Assert.assertEquals("Dollars to Canadian Dollar", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToSingaporeanDollar(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 715;
+        //When
+        double actual = calculate.convertUSDToSingaporeanDollar(usd);
+        //Then
+        Assert.assertEquals("Dollars to Singaporean Dollar", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToRinggit(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 2235;
+        //When
+        double actual = calculate.convertUSDToRinggit(usd);
+        //Then
+        Assert.assertEquals("Dollars to Ringgit ", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToYen(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 57920;
+        //When
+        double actual = calculate.convertUSDToYen(usd);
+        //Then
+        Assert.assertEquals("Dollars to Yen", expectedSum, actual,.0000d);
+
+    }
+
+    @Test
+    public void testConvertUSDToYuan(){
+        //Given
+        Calculate calculate = new Calculate();
+        double usd = 500;
+        double expectedSum = 3460;
+        //When
+        double actual = calculate.convertUSDToYuan(usd);
+        //Then
+        Assert.assertEquals("Dollars to Ringgit ", expectedSum, actual,.0000d);
 
     }
 }
