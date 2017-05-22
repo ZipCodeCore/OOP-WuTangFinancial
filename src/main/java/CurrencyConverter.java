e80fe148b887bec6a6b8c66b5b02b0e8044e6456
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-22 16:36:40 -0400

    Initial commit

---
+++ src/main/java/CurrencyConverter.java
@@ -0,0 +1,5 @@
+/**
+ * Created by rahmirford on 5/22/17.
+ */
+public class CurrencyConverter {
+}
---
+++ src/main/test/TestCurrencyConverter.java
@@ -0,0 +1,6 @@
+/**
+ * Created by rahmirford on 5/22/17.
+ */
+public class TestCurrencyConverter {
+
+}
