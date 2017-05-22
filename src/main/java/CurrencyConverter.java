e48fc9cee994cb246e49852e682c5670e14b21eb
Brian Mullin <brmullin@email.msmary.edu>
2017-05-22 17:24:20 -0400

    Set up empty mthods and tests

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -1,7 +1,39 @@
 /**
  * Created by brianmullin on 5/22/17.
  */
+import java.util.Map;
+import java.util.Map;
+import java.util.TreeMap;
+import java.math.BigDecimal;
+
 public class CurrencyConverter {
+
+    Map<String, Long> conversionMap = new TreeMap<String, Long>() {{put("USD", 1.00); put("Euro", 0.94);
+                                put("POUND", 0.82); put("AUS", 1.35); put("RUP", 68.32); put("CAN", 1.32);
+                                put("SING", 1.43); put("FRANC", 1.01); put("RINGGIT", 4.47); put("YEN", 115.84); put("YUAN", 6.92)}}
+
+
+    public long convertTo(long firstRate, long secondRate, long amount){
+
+        return convertedAmount;
+    }
+
+    public long getFirstExchangeRate(String country){
+
+        long firstRate = conversionMap.get(country);
+
+        return firstRate;
+    }
+
+    public long getSecondExchangeRate(String country){
+
+        long secondRate = conversionMap.get(country);
+
+        return secondRate;
+    }
+
+
+
 
 
 }
---
+++ src/test/java/CurrencyConverterTest.java
@@ -0,0 +1,119 @@
+/**
+ * Created by brianmullin on 5/22/17.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CurrencyConverterTest {
+
+    CurrencyConverter converter;
+
+    @Before
+
+    public void setUp() {
+        converter = new CurrencyConverter();
+    }
+
+    @Test
+    public void testConvertDollarToEuro(){ //dollar to euro
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testConvertEuroToDollar(){  //Euro to dollar
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testConvertEuroToBritishPound(){  //Eur to Pound
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testConvertBritishPoundToIndianRupee(){ //Pound to Rupee
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testConvertRupeeToCanadianDollar(){ //Rupee to Can
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testCanadianDollarToSingaporeDollar(){ //Can to Sing
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testSingaporeDollarToSwissFranc(){ //Sing to Franc
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testConvertSwissFrancToMalaysianRinggit(){ //Franc to Ringgit
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testConvertRinggitToJapaneseYen(){ //Ringgit to Yen
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+    @Test
+    public void testConvertJapanesYenToChineseYuanRenminbi(){  //Yen to Yuan
+        //:Given
+
+        //:When
+
+        //:Then
+
+    }
+
+}
