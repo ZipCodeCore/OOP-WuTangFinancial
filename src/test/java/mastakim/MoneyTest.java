03d43d2911654a535c37750793c505f21ad58dcd
Chris <chriskim80@gmail.com>
2017-05-22 16:45:07 -0400

    Created test stubs for Money

--- src/test/java/mastakim/MoneyTest.java
+++ src/test/java/mastakim/MoneyTest.java
@@ -5,4 +5,110 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MoneyTest {
+
+    @Before
+    public void initialize() {
+
+    }
+
+
+    //Convert USD to EUR
+    @Test
+    public void convertUSDtoEURTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert EUR to USD
+    @Test
+    public void convertEURtoUSDTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert EUR to GBP
+    @Test
+    public void convertEURtoGBPTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert GBP to INR
+    @Test
+    public void convertGBPtoINRTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert INR to CAD
+    @Test
+    public void convertINRtoCADTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert CAD to SGD
+    @Test
+    public void convertCADtoSGDTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert SGD to CHF
+    @Test
+    public void convertSGDtoCHFTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert CHF to MYR
+    @Test
+    public void convertCHFtoMYRTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert MYR to JPY
+    @Test
+    public void convertMYRtoJPYTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
+
+    //Convert JPY to CNY
+    @Test
+    public void convertJPYtoCNYTest(){
+        //Given
+
+        //When
+
+        //Then
+    }
 }
