bf7d890be8172898e01abf2efe96c98ee4541546
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-22 18:00:08 -0400

    added alot of stuff

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -8,26 +8,42 @@ import java.math.BigDecimal;
 
 public class CurrencyConverter {
 
-    Map<String, Long> conversionMap = new TreeMap<String, Long>() {{put("USD", 1.00); put("Euro", 0.94);
-                                put("POUND", 0.82); put("AUS", 1.35); put("RUP", 68.32); put("CAN", 1.32);
-                                put("SING", 1.43); put("FRANC", 1.01); put("RINGGIT", 4.47); put("YEN", 115.84); put("YUAN", 6.92)}}
+    Map<String, Double> conversionMap = new TreeMap<String, Double>() {{put("USD", 1.00); put("EURO", 0.94);
+        put("POUND", 0.82); put("AUS", 1.35); put("RUP", 68.32); put("CAN", 1.32); put("SING", 1.43);
+        put("FRANC", 1.01); put("RINGGIT", 4.47); put("YEN", 115.84); put("YUAN", 6.92)}};
 
 
-    public long convertTo(long firstRate, long secondRate, long amount){
+    public String firstCountry;
+    public String secondCountry;
+    public double amount;
+    public double firstRate;
+    public double secondRate;
+
+
+    public CurrencyConverter(String firstCountry, String secondCountry, double amount){
+
+        this.firstCountry = firstCountry;
+        this.secondCountry = secondCountry;
+        this.amount = amount;
+    }
+
+    public double convertTo(String firstCountry, String secondCountry, double amount){
+
+        double convertedAmount = (getFirstExchangeRate(firstCountry)/getSecondExchangeRate(secondCountry)) * amount;
 
         return convertedAmount;
     }
 
-    public long getFirstExchangeRate(String country){
+    public double getFirstExchangeRate(String country){
 
-        long firstRate = conversionMap.get(country);
+        this.firstRate = conversionMap.get(country);
 
         return firstRate;
     }
 
-    public long getSecondExchangeRate(String country){
+    public double getSecondExchangeRate(String country){
 
-        long secondRate = conversionMap.get(country);
+        this.secondRate = conversionMap.get(country);
 
         return secondRate;
     }
