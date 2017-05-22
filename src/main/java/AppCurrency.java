d403949441f36abd9c2912347e019cdc0d0e78c9
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-22 17:24:20 -0400

    Set up empty mthods and tests

--- src/main/java/AppCurrency.java
+++ src/main/java/AppCurrency.java
@@ -1,5 +1,20 @@
 /**
  * Created by brianmullin on 5/22/17.
  */
+
+import java.util.Scanner;
+
 public class AppCurrency {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Wu Tang Financial's Official Currency Calculator powered by WorldStarHipHop's Java Engine");
+
+        int choice = input.nextInt();
+
+        System.out.println(choice);
+
+    }
 }
