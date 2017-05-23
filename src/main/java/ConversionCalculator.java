d7ba1c0f3f706a212c7000c141577073419f6903
Molly Arant <mccormick.mollyt@gmail.com>
2017-05-22 20:33:25 -0400

    I realize there is an entirely more efficient  way to do this... but the ten tests listed as must haves  definitely run.

--- src/main/java/ConversionCalculator.java
+++ src/main/java/ConversionCalculator.java
@@ -2,4 +2,67 @@
  * Created by mollyarant on 5/22/17.
  */
 public class ConversionCalculator {
+    ConversionToolToUSDollar convertToDollar = new ConversionToolToUSDollar();
+    ConversionToolFromUSDollar convertFromDollar= new ConversionToolFromUSDollar();
+
+    public double calculateCurrencyEuroToPound(double amount){
+        double dollarConversion= convertToDollar.convertEuroToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToPound(dollarConversion);
+        return result;
+    }
+
+    public double calculateCurrencyEuroToRupee(double amount){
+        double dollarConversion= convertToDollar.convertEuroToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToIndianRupee(dollarConversion);
+        return result;
+    }
+
+    public double calculateCurrencyPoundToRupee(double amount){
+        double dollarConversion= convertToDollar.convertPoundToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToIndianRupee(dollarConversion);
+        return result;
+    }
+
+    public double calculateCurrencyRupeeToCanadianDollar(double amount){
+        double dollarConversion= convertToDollar.convertIndianRupeeToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToCanadianDollar(dollarConversion);
+        return result;
+    }
+
+    public double calculateCurrencyCanadianDollarToSingaporeDollar(double amount){
+        double dollarConversion= convertToDollar.convertCanadianDollarToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToSingaporeDollar(dollarConversion);
+        return result;
+    }
+    public double calculateCurrencySingaporeDollarToMalaysianRinggit(double amount){
+        double dollarConversion= convertToDollar.convertSingaporeDollarToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToMalaysianRinggit(dollarConversion);
+        return result;
+    }
+    public double calculateCurrencySingaporeDollarToSwissFranc(double amount){
+        double dollarConversion= convertToDollar.convertSingaporeDollarToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToSwissFranc(dollarConversion);
+        return result;
+    }
+    public double calculateCurrencySwissFranctoMalaysianRinggit(double amount){
+        double dollarConversion= convertToDollar.convertSwissFrancToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToMalaysianRinggit(dollarConversion);
+        return result;
+    }
+
+    public double calculateCurrencyRinggitToYen(double amount){
+        double dollarConversion= convertToDollar.convertMalaysianRinggitToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToJapaneseYen(dollarConversion);
+        return result;
+    }
+
+    public double calculateCurrencyYenToYuan(double amount){
+        double dollarConversion= convertToDollar.convertJapaneseYenToUSDollar(amount);
+        double result= convertFromDollar.convertDollarToChineseYuan(dollarConversion);
+        return result;
+    }
+
+
+
+
 }
--- src/test/java/TestConversionCalculator.java
+++ src/test/java/TestConversionCalculator.java
@@ -1,7 +1,118 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by mollyarant on 5/22/17.
  */
 public class TestConversionCalculator {
-    
+    ConversionCalculator conversionCalculator;
+
+    @Before
+    public void setUp() {
+        conversionCalculator = new ConversionCalculator();
+    }
+
+    @Test
+    public void calculateCurrencyEuroToPound() {
+        //given
+        double amountToConvert = 10.00;
+        //when
+        double actual = conversionCalculator.calculateCurrencyEuroToPound(amountToConvert);
+
+        //then
+        Assert.assertEquals("should return euros converted to a pound", 8.72, actual, 0.01);
+    }
+
+    @Test
+    public void calculateCurrencyEuroToRupee(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencyEuroToRupee(amountToConvert);
+        //then
+        Assert.assertEquals("should return euros converted to rupees",726.80, actual, 0.01 );
+    }
+
+    @Test
+    public void calculateCurrencyPoundToRupee(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencyPoundToRupee(amountToConvert);
+        //then
+        Assert.assertEquals("should return pounds to rupees",833.17, actual, 0.01 );
+    }
+
+    @Test
+    public void calculateCurrencyRupeetoCanadianDollar(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencyRupeeToCanadianDollar(amountToConvert);
+        //then
+        Assert.assertEquals("should return Rupees converted to canadian dollars",0.19, actual, 0.01 );
+    }
+
+    @Test
+    public void calculateCurrencyCanadianToSingapore(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencyCanadianDollarToSingaporeDollar(amountToConvert);
+        //then
+        Assert.assertEquals("should return canadian dollars converted to Singapore dollars",10.833, actual, 0.01 );
+    }
+
+    @Test
+    public void calculateCurrencySingaporeToMalaysia(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencySingaporeDollarToMalaysianRinggit(amountToConvert);
+        //then
+        Assert.assertEquals("should return euros converted to rupees",31.25, actual, 0.01 );
+    }
+
+    @Test
+    public void calculateCurrencySingaporeToSwissFranc(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencySingaporeDollarToSwissFranc(amountToConvert);
+        //then
+        Assert.assertEquals("should return  singapore dollars to swiss franc",7.06, actual, 0.01 );
+    }
+    @Test
+    public void calculateCurrencySwissFrancToRinggit(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencySwissFranctoMalaysianRinggit(amountToConvert);
+        //then
+        Assert.assertEquals("should return  franc to ringgit",44.25, actual, 0.01 );
+    }
+
+    @Test
+    public void calculateCurrencyRinggitToYen(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencyRinggitToYen(amountToConvert);
+        //then
+        Assert.assertEquals("should return Ringgit to Yen",259.15, actual, 0.01 );
+    }
+
+    @Test
+    public void calculateCurrencyYentoYuan(){
+        //given
+        double amountToConvert=10.00;
+        //when
+        double actual= conversionCalculator.calculateCurrencyYenToYuan(amountToConvert);
+        //then
+        Assert.assertEquals("should return yen to yuan",0.60, actual, 0.01 );
+    }
+
+
 
 }
