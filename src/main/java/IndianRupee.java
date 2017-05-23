bbc84a44031263781200d540f6162cce7919f4e6
waxdoll23 <jmcgnty23@gmail.com>
2017-05-22 22:24:08 -0400

    Finished the Rupee to Australian Dollar conversion

--- src/main/java/IndianRupee.java
+++ src/main/java/IndianRupee.java
@@ -1,5 +1,14 @@
+
+
 /**
  * Created by jennifermcginty on 5/22/17.
  */
 public class IndianRupee {
+
+    public double indianRupeeToAustralianDollars(double input){
+        double finalConversion = input * 50.60;
+        return finalConversion;
+    }
+
+
 }
--- src/test/java/IndianRupeeTest.java
+++ src/test/java/IndianRupeeTest.java
@@ -1,5 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by jennifermcginty on 5/22/17.
  */
 public class IndianRupeeTest {
+
+    @Test
+    public void indianRupeeToAustralianDollarsTest(){
+        IndianRupee indianRupee = new IndianRupee();
+
+        double expected = 4260.52;
+        double actual = indianRupee.indianRupeeToAustralianDollars(84.20);
+
+        Assert.assertEquals("This should return 4260.52", expected, actual,.01);
+    }
 }
