3714b8536dd39213fccfc6badcb576a0665f2536
waxdoll23 <jmcgnty23@gmail.com>
2017-05-22 20:24:12 -0400

    Finished Euro to British Pound.

--- src/main/java/EuroConversion.java
+++ src/main/java/EuroConversion.java
@@ -2,4 +2,10 @@
  * Created by jennifermcginty on 5/22/17.
  */
 public class EuroConversion {
+
+    public double euroToBritishPound(double input){
+        double finalConversion = input * .87;
+        return finalConversion;
+    }
+
 }
--- src/test/java/EuroConversionTest.java
+++ src/test/java/EuroConversionTest.java
@@ -1,5 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by jennifermcginty on 5/22/17.
  */
 public class EuroConversionTest {
+
+    @Test
+    public void euroToBritishPoundTest(){
+        EuroConversion euroConversion = new EuroConversion();
+
+        double expected =  8.70;
+        double actual = euroConversion.euroToBritishPound(10);
+
+        Assert.assertEquals("This should return 8.70", expected ,actual, .01);
+    }
 }
