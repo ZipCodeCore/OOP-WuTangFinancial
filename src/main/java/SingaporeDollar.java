1df9864fcb4834a41a657a5b03bdeb68d9f99fb5
waxdoll23 <jmcgnty23@gmail.com>
2017-05-23 00:20:26 -0400

    Finished Singapore Dollar to Franc.

--- src/main/java/SingaporeDollar.java
+++ src/main/java/SingaporeDollar.java
@@ -2,4 +2,9 @@
  * Created by jennifermcginty on 5/23/17.
  */
 public class SingaporeDollar {
+
+    public double singaporeDollarToSwissFranc(double input){
+        double finalConversion = input * 1.41;
+        return finalConversion;
+    }
 }
--- src/test/java/SingaporeDollarTest.java
+++ src/test/java/SingaporeDollarTest.java
@@ -1,5 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by jennifermcginty on 5/23/17.
  */
 public class SingaporeDollarTest {
+    @Test
+    public void singaporeDollarToSwissFrancTest(){
+        SingaporeDollar singaporeDollar = new SingaporeDollar();
+
+        double expected = 39.48;
+        double acutal = singaporeDollar.singaporeDollarToSwissFranc(28);
+
+        Assert.assertEquals("This should return 39.48", expected, acutal, .01);
+    }
 }
