fea4a9c7ad09d79ce1044c057b31c20adcee498b
waxdoll23 <jmcgnty23@gmail.com>
2017-05-22 23:44:13 -0400

    Finished Canadian Dollar to Singapore Dollar.

--- src/main/java/CanadianDollar.java
+++ src/main/java/CanadianDollar.java
@@ -2,4 +2,11 @@
  * Created by jennifermcginty on 5/22/17.
  */
 public class CanadianDollar {
+
+    public double canadianDollarToSingaporeDollar(double input){
+        double finalConversion = input * 1.08;
+        return finalConversion;
+    }
+
+
 }
--- src/test/java/CanadianDollarTest.java
+++ src/test/java/CanadianDollarTest.java
@@ -1,6 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by jennifermcginty on 5/22/17.
  */
 public class CanadianDollarTest {
-    
+    @Test
+    public void canadianDollarToSingaporeDollarTest(){
+        CanadianDollar canadianDollar = new CanadianDollar();
+
+        double expected = 82.08;
+        double actual = canadianDollar.canadianDollarToSingaporeDollar(76);
+
+        Assert.assertEquals("This should return 82.08", expected, actual, .01);
+    }
 }
