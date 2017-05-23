0223afb9d44da5c3135be9c226ad18f84188421a
faethflex <gfletchjr@gmail.com>
2017-05-23 06:36:46 -0400

    last commit didn't add all the files, no changes since last commit

---
+++ wutang/src/main/java/GreatBritainPound.java
@@ -0,0 +1,15 @@
+/**
+ * Created by gregoryfletcher on 5/22/17.
+ */
+public class GreatBritainPound extends Currency
+{
+	public GreatBritainPound()
+	{
+		super("GBP", 1.0, 0.84);
+	}
+
+	public GreatBritainPound(double amount)
+	{
+		super("GBP", amount, 0.84);
+	}
+}
