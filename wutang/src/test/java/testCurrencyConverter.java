617c39a059c24400739639420548569a0eb35b12
faethflex <gfletchjr@gmail.com>
2017-05-22 21:47:13 -0400

    started on tests for conversions

--- wutang/src/test/java/testCurrencyConverter.java
+++ wutang/src/test/java/testCurrencyConverter.java
@@ -8,30 +8,38 @@ import static org.junit.Assert.assertEquals;
  */
 public class testCurrencyConverter
 {
-	public USDollar dollar;
-	public Euro euro;
-	public CurrencyConverter crooks;
+	public AustralianDollar aud;
+	public GreatBritainPound gbp;
+	public CanadianDollar cad;
+	public ChineseYuanRenminbi cnyrmb;
+	public IndianRupee inr;
+	public JapaneseYen jpy;
+	public MalaysianRinggit myr;
+	public SingaporeDollar sgd;
+	public SwissFranc chf;
+	public CurrencyConverter conversionEngine;
 
 	@Before
 	public void startup()
 	{
-		dollar = new USDollar();
-		euro = new Euro();
-		crooks = new CurrencyConverter();
+
+		conversionEngine = new CurrencyConverter();
 	}
 
 	@Test
-	public void testConvertCurrency()
+	public void testConvertCurrencyDollarToEuro()
 	{
 		//: Given
-		crooks.convertTo(euro);
-		crooks.convertFrom(dollar);
+		USDollar usd = new USDollar();
+		Euro eur = new Euro();
+		conversionEngine.convertTo(eur);
+		conversionEngine.convertFrom(usd);
 
 		double expected = 0.94;
 
 		//: When
-		crooks.convertCurrency();
-		Currency newEuros = crooks.convertedCurrency;
+		conversionEngine.convertCurrency();
+		Currency newEuros = conversionEngine.convertedCurrency;
 		double actual = newEuros.getAmount();
 
 		//: Then
@@ -39,7 +47,16 @@ public class testCurrencyConverter
 	}
 
 	@Test
-	public void test
+	public void testConvertCurrencyEuroToDollar()
+	{
+		//: Given
+		USDollar usd = new USDollar();
+		Euro eur = new Euro();
+		conversionEngine.convertTo(usd);
+		conversionEngine.convertFrom(eur);
+
+		double expected = 0.94;
+	}
 
 
 }
