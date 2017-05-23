89ded9fc8afa4c9be0a45caae0d29526a0f4ae60
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-05-22 21:00:28 -0400

    Commented out userInput

--- wutang/src/main/java/Input.java
+++ wutang/src/main/java/Input.java
@@ -2,27 +2,27 @@
  * Created by jarrydstamatelos on 5/22/17.
  */
 public class Input {
-
-    public void getstartCurrency() {
-        Console.getDoubleInput("Select from the following options by number: \n" +
-                "(1) UsDollar \n" +
-                "(2) BritishPound \n" +
-                "(3) Indian Rupee \n" +
-                "(4) Aussie Dollar \n" +
-                "(5) Canada Dollar \n" +
-                "(6) Singapore Dollar \n" +
-                "(7) Swiss Franc \n" +
-                "(8) Ringgit \n" +
-                "(9) Yen \n" +
-                "(10) Yuan");
-
-    }
+//
+//    public void getstartCurrency() {
+//        Console.getDoubleInput("Select from the following options by number: \n" +
+//                "(1) UsDollar \n" +
+//                "(2) BritishPound \n" +
+//                "(3) Indian Rupee \n" +
+//                "(4) Aussie Dollar \n" +
+//                "(5) Canada Dollar \n" +
+//                "(6) Singapore Dollar \n" +
+//                "(7) Swiss Franc \n" +
+//                "(8) Ringgit \n" +
+//                "(9) Yen \n" +
+//                "(10) Yuan");
+//
+//    }
 
 
     public static void main(String[] args) {
 
         Currency wuTang = new Currency();
-        // starting currency --> target currency
+
         Console.printDouble(wuTang.CurrencyConversion(Currency.USDOLLAR, Currency.AUSSIEDOLLAR));
         Console.printDouble(wuTang.CurrencyConversion(Currency.AUSSIEDOLLAR, Currency.SWISSFRANC));
         Console.printDouble(wuTang.CurrencyConversion(Currency.CHINESEYUAN, Currency.USDOLLAR));
