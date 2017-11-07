a7a6a84632b2647000c8ece99d03881e0ad88e59
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-07 11:26:05 -0500

    currency type enum class

---
+++ src/main/java/CurrencyType.java
@@ -0,0 +1,23 @@
+public enum CurrencyType {
+    US_DOLLAR (1.00),
+    EURO (0.94),
+    BRITISH_POUND (0.82),
+    INDIAN_RUPEE (68.32),
+    AUSTRALIAN_DOLLAR (1.35),
+    CANADIAN_DOLLAR (1.32),
+    SINGAPORE (1.43),
+    SWISS_FRANC (1.01),
+    MALAYSIAN_RINGGIT (4.47),
+    JAPANESE_YEN (115.84),
+    CHINESE_YUAN_RENMINBI (6.92);
+
+    private final double rateWRTUSDollar;
+
+    CurrencyType(double rateWRTUSDollar) {
+        this.rateWRTUSDollar = rateWRTUSDollar;
+    }
+
+    public double getRateWRTUSDollar() {
+        return rateWRTUSDollar;
+    }
+}
