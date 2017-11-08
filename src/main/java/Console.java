2f90ff4074c81ee1043849e011e8c603aaa8d9ad
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-11-08 13:46:13 -0500

    Updated with exit choice/loop

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -3,53 +3,50 @@ import java.util.Scanner;
 
 public class Console {
 
-    public void startMainMenu() {
+    public static void startMainMenu() {
         System.out.print(
                 "**************** [Welcome to Currency Converter!] ******************\n" +
-                        "Below are the currency choices you can convert from: \n" +
-                        "[USD] [EURO] [POUND] [RUPEE] [AUSTRALIAN DOLLAR] [CANADIAN DOLLAR]\n" +
-                        "[SINGAPORE DOLLAR] [FRANC] [RINGGIT] [YEN] [YUAN REMINBI]\n" +
-                        "*********************** [Type EXIT to exit] ************************\n");
+                "Below are the currency choices you can convert from: \n" +
+                "[USD] [EURO] [POUND] [RUPEE] [AUSTRALIAN DOLLAR] [CANADIAN DOLLAR]\n" +
+                "[SINGAPORE DOLLAR] [FRANC] [RINGGIT] [YEN] [YUAN REMINBI]\n" +
+                "*********************************************************************\n\n");
     }
 
-    public static String getStringInput(String prompt) {
+    public String getStringInput(String prompt) {
         Scanner sc = new Scanner(System.in);
         System.out.print(prompt);
         String userInput = sc.nextLine();
         return userInput;
     }
 
-    public ExchangeRates getExchangeType(String prompt) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print(prompt);
-        String userInput = sc.next();
+    public ExchangeRates getExchangeType(String userInput) {
         ExchangeRates exchangeRates = ExchangeRates.valueOf(userInput.toUpperCase());
         switch (exchangeRates) {
-            case USD:
-                return ExchangeRates.USD;
-            case EURO:
-                return ExchangeRates.EURO;
-            case POUND:
-                return ExchangeRates.POUND;
-            case RUPEE:
-                return ExchangeRates.RUPEE;
-            case AUSTRALIANDOLLAR:
-                return ExchangeRates.AUSTRALIANDOLLAR;
-            case CANADIANDOLLAR:
-                return ExchangeRates.CANADIANDOLLAR;
-            case SINGAPOREDOLLAR:
-                return ExchangeRates.SINGAPOREDOLLAR;
-            case FRANC:
-                return ExchangeRates.FRANC;
-            case RINGGIT:
-                return ExchangeRates.RINGGIT;
-            case YEN:
-                return ExchangeRates.YEN;
-            case YUANREMINBI:
-                return ExchangeRates.YUANREMINBI;
-            default:
-                System.out.println("Please enter a valid option!");
-                return exchangeRates;
+                    case USD:
+                        return ExchangeRates.USD;
+                    case EURO:
+                        return ExchangeRates.EURO;
+                    case POUND:
+                        return ExchangeRates.POUND;
+                    case RUPEE:
+                        return ExchangeRates.RUPEE;
+                    case AUSTRALIANDOLLAR:
+                        return ExchangeRates.AUSTRALIANDOLLAR;
+                    case CANADIANDOLLAR:
+                        return ExchangeRates.CANADIANDOLLAR;
+                    case SINGAPOREDOLLAR:
+                        return ExchangeRates.SINGAPOREDOLLAR;
+                    case FRANC:
+                        return ExchangeRates.FRANC;
+                    case RINGGIT:
+                        return ExchangeRates.RINGGIT;
+                    case YEN:
+                        return ExchangeRates.YEN;
+                    case YUANREMINBI:
+                        return ExchangeRates.YUANREMINBI;
+                    default:
+                        System.out.println("Please enter a valid option!");
+                        return exchangeRates;
         }
     }
 
@@ -65,12 +62,19 @@ public class Console {
             }
         } while (true);
     }
-}
 
-//to update with:
-//what if the user inputs the wrong currency?
-//what if the user puts in a negative number?
-//what if the user wants to do another conversion/stop conversion?
+    public boolean exitOrContinueApplication() {
+        String ans = getStringInput("Would you like to exit the application: YES or NO?");
+        if ("YES".equalsIgnoreCase(ans)) {
+            System.out.println("Thank you! Have a nice day!");
+            System.exit(0);
+        }
+        else if ("NO".equalsIgnoreCase(ans)) {
+            startMainMenu();
+        }
+        return true;
+    }
+}
 
 //if using "return" in switch statement, you don't need a break. b/c return will return a result a complete the method.
 //break will break out of the current scope of {curly brackets} and enter above into a large capsulated scope of {curly brackets}.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    Console console = new Console();
-    console.startMainMenu();
+        Console console = new Console();
+        Console.startMainMenu();
+        do {
+            ExchangeRates currencyToChoice = console.getExchangeType(
+                    console.getStringInput("What currency would you like to convert to?"));
 
-    ExchangeRates currencyToChoice = console.getExchangeType("What currency would you like to convert to?");
-    ExchangeRates currencyFromChoice = console.getExchangeType("Looking at the menu, what currency are you converting from?");
-    System.out.println("How much would you like to convert?");
-    double amount = sc.nextInt();
+            ExchangeRates currencyFromChoice = console.getExchangeType(
+                    console.getStringInput("Looking at the menu, what currency are you converting from?"));
 
-    CurrencyConverter converter = new CurrencyConverter();
-    double convertedAmount = converter.convertCurrency(amount, currencyFromChoice, currencyToChoice);
-        System.out.printf("$%.2f %s is $%.2f in %s!", amount, currencyFromChoice, convertedAmount, currencyToChoice);
+            double amount = console.getDoubleInput("How much would you like to convert?");
+
+            CurrencyConverter converter = new CurrencyConverter();
+            double convertedAmount = converter.convertCurrency(amount, currencyFromChoice, currencyToChoice);
+            System.out.printf("$%.2f %s is $%.2f in %s!", amount, currencyFromChoice, convertedAmount, currencyToChoice);
+
+        } while (console.exitOrContinueApplication());
     }
 }
-
