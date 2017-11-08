4ef44dbf323fe70c960f46dd05aabe1bcfd1e130
aaron <aaron@zipcoders-MacBook-Pro.local>
2017-11-08 13:31:43 -0500

    Aaron Herion - Updated submission - Minor revision

--- src/test/java/Conversion_Logic_Test.java
+++ src/test/java/Conversion_Logic_Test.java
@@ -16,7 +16,7 @@ public class Conversion_Logic_Test {
         double expected = 0.94;
         double actual = conversion.exchange(0, 1);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -29,7 +29,7 @@ public class Conversion_Logic_Test {
         double expected = 0.94;
         double actual = conversion.exchange(1, 0);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
 
     }
@@ -43,7 +43,7 @@ public class Conversion_Logic_Test {
         double expected = 0.77;
         double actual = conversion.exchange(1, 2);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -56,7 +56,7 @@ public class Conversion_Logic_Test {
         double expected = 56.02;
         double actual = conversion.exchange(2, 3);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -69,7 +69,7 @@ public class Conversion_Logic_Test {
         double expected = 92.23;
         double actual = conversion.exchange(3, 4);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -82,7 +82,7 @@ public class Conversion_Logic_Test {
         double expected = 1.78;
         double actual = conversion.exchange(4, 5);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -95,7 +95,7 @@ public class Conversion_Logic_Test {
         double expected = 1.44;
         double actual = conversion.exchange(6, 7);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -108,7 +108,7 @@ public class Conversion_Logic_Test {
         double expected = 4.51;
         double actual = conversion.exchange(7, 8);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -121,7 +121,7 @@ public class Conversion_Logic_Test {
         double expected = 517.80;
         double actual = conversion.exchange(8, 9);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
@@ -134,7 +134,7 @@ public class Conversion_Logic_Test {
         double expected = 801.61;
         double actual = conversion.exchange(9, 10);
 
-        Assert.assertEquals(expected, actual, 1);
+        Assert.assertEquals(expected, actual, .01);
 
     }
 
