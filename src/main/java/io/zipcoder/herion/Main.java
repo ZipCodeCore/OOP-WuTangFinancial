f6bf6305a2c863205789d652802a2a3976db651f
aaron <aaronherion111@gmail.com>
2017-12-17 03:09:06 -0500

    minor updates

--- src/main/java/io/zipcoder/herion/Main.java
+++ src/main/java/io/zipcoder/herion/Main.java
@@ -1,5 +1,9 @@
 package io.zipcoder.herion;
 
+//This is the solution to one of the labs early on in Zip Code Wilmington course, in which the requirement was to create a
+//program which could convert currency from among ten specified currency types with current rates fixed
+
+
 public class Main {
 
     public static void main(String[] args) {
