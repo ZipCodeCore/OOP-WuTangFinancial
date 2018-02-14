1d49f596d382dfce015954b46490127a60581774
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-14 13:31:39 -0500

    done

--- src/main/java/Rate.java
+++ src/main/java/Rate.java
@@ -1,16 +1,19 @@
 public class Rate {
+
     //created finals because the rates do not change and no one else needs to change them
-    private static double final USD = 1.00;
-    private static double final EUR = 0.94;
-    private static double final GBP = 0.82;
-    private static double final INR = 68.32;
-    private static double final AUD = 1.35;
-    private static double final CAD = 1.32;
-    private static double final SGD = 1.43;
-    private static double final CHF = 1.01;
-    private static double final MYR = 4.47;
-    private static double final JPY = 115.84;
-    private static double final CNY = 6.92;
+    private static final double USD = 1.00;
+    private static final double EUR = 0.94;
+    private static final double GBP = 0.82;
+    private static final double INR = 68.32;
+    private static final double AUD = 1.35;
+    private static final double CAD = 1.32;
+    private static final double SGD = 1.43;
+    private static final double CHF = 1.01;
+    private static final double MYR = 4.47;
+    private static final double JPY = 115.84;
+    private static final double CNY = 6.92;
+
+
     //created my action that will convert the rates and be used in RateTest
     public double rateConverter(double value, String start, String end){
 
@@ -18,11 +21,11 @@ public class Rate {
 
     }
     //used a switch statement to hold all the values and go through each one.
-    private double rateOfExchange(String rateConverter){
+    private double getRateOfExchange(String rateConverter){
 
-        switch (currency){
+        switch (rateConverter){
             case "USD":
-                return USD;
+            return USD;
 
             case "EUR":
                 return EUR;
--- src/test/java/RateTest.java
+++ src/test/java/RateTest.java
@@ -18,7 +18,133 @@ public class RateTest {
         Rate rate = new Rate();
 
         //THEN
-        double results = rate.rateOfExchange(initialValue, startingCurrency, endingCurrency);
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
         Assert.assertEquals(expectedValue, results, .000000001);
-        }
+    }
+
+    @Test
+    public void convertEuroToDollar() {
+    /*So just like the other test we created, we want the one value to test against another and then we specify which
+        country those values are from. */
+        //GIVEN
+        double initialValue = .94;
+        double expectedValue = 1;
+        String startingCurrency = "EUR";
+        String endingCurrency = "USD";
+
+        //WHEN
+        Rate rate = new Rate();
+
+        //THEN
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
+        Assert.assertEquals(expectedValue, results, .000000001);
+    }
+
+    @Test
+    public void convertEuroToGreatBritishPd() {
+    /*So just like the other test we created, we want the one value to test against another and then we specify which
+        country those values are from. */
+        //GIVEN
+        double initialValue = .94;
+        double expectedValue = .82;
+        String startingCurrency = "EUR";
+        String endingCurrency = "GBP";
+
+        //WHEN
+        Rate rate = new Rate();
+
+        //THEN
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
+        Assert.assertEquals(expectedValue, results, .000000001);
+    }
+
+    @Test
+    public void convertIndianRToCanadianDollar() {
+    /*So just like the other test we created, we want the one value to test against another and then we specify which
+        country those values are from. */
+        //GIVEN
+        double initialValue = 68.32;
+        double expectedValue = 1.32;
+        String startingCurrency = "INR";
+        String endingCurrency = "CAD";
+
+        //WHEN
+        Rate rate = new Rate();
+
+        //THEN
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
+        Assert.assertEquals(expectedValue, results, .000000001);
+    }
+
+    @Test
+    public void convertCanadianDollarToSwissFranc() {
+    /*So just like the other test we created, we want the one value to test against another and then we specify which
+        country those values are from. */
+        //GIVEN
+        double initialValue = 1.32;
+        double expectedValue = 1.01;
+        String startingCurrency = "CAD";
+        String endingCurrency = "CHF";
+
+        //WHEN
+        Rate rate = new Rate();
+
+        //THEN
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
+        Assert.assertEquals(expectedValue, results, .000000001);
+    }
+
+    @Test
+    public void convertSwissFrancToMalayR() {
+    /*So just like the other test we created, we want the one value to test against another and then we specify which
+        country those values are from. */
+        //GIVEN
+        double initialValue = 1.01;
+        double expectedValue = 4.47;
+        String startingCurrency = "CHF";
+        String endingCurrency = "MYR";
+
+        //WHEN
+        Rate rate = new Rate();
+
+        //THEN
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
+        Assert.assertEquals(expectedValue, results, .000000001);
+    }
+
+    @Test
+    public void convertMalayRToJYen() {
+    /*So just like the other test we created, we want the one value to test against another and then we specify which
+        country those values are from. */
+        //GIVEN
+        double initialValue = 4.47;
+        double expectedValue = 115.84;
+        String startingCurrency = "MYR";
+        String endingCurrency = "JPY";
+
+        //WHEN
+        Rate rate = new Rate();
+
+        //THEN
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
+        Assert.assertEquals(expectedValue, results, .000000001);
+    }
+
+    @Test
+    public void convertJYenToChineseYR() {
+    /*So just like the other test we created, we want the one value to test against another and then we specify which
+        country those values are from. */
+        //GIVEN
+        double initialValue = 115.84;
+        double expectedValue = 6.92;
+        String startingCurrency = "JPY";
+        String endingCurrency = "CNY";
+
+        //WHEN
+        Rate rate = new Rate();
+
+        //THEN
+        double results = rate.rateConverter(initialValue, startingCurrency, endingCurrency);
+        Assert.assertEquals(expectedValue, results, .000000001);
+    }
 }
