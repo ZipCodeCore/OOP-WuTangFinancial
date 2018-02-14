a7b3c086b8f2aa21c0a65a60f72b48e2856a2c97
Lawrence Wu <lawrencewu@zipcoders-MacBook-Pro-3.local>
2018-02-13 23:30:01 -0500

    Nvm got it done. Need to work on the delta though

--- src/main/java/com/zipcodewilmington/ExchangeRate/ExchangeRate.java
+++ src/main/java/com/zipcodewilmington/ExchangeRate/ExchangeRate.java
@@ -1,43 +1,40 @@
 package com.zipcodewilmington.ExchangeRate;
 
 public enum ExchangeRate {
-    USD(1.00),
-    GBP(0.82),
-    EUR(0.94),
-    INR(68.32),
-    AUD(1.35),
-    CAD(1.32),
-    SGD(1.43),
-    CHF(1.01),
-    MYR(4.47),
-    JPY(115.84),
-    CNY(6.92);
+        USD(1.00f),
+        GBP(0.82f),
+        EUR(0.94f),
+        INR(68.32f),
+        AUD(1.35f),
+        CAD(1.32f),
+        SGD(1.43f),
+        CHF(1.01f),
+        MYR(4.47f),
+        JPY(115.84f),
+        CNY(6.92f);
 
-    public double rate;
+        public double rate;
 
-    public double getRate() {
-        return rate;
-    }
+        public double getRate() {
+            return rate;
+        }
 
-    public void setRate(double rate) {
-        this.rate = rate;
-    }
+        public void setRate(double rate) {
+            this.rate = rate;
+        }
 
-    ExchangeRate(double rate) {
-        this.rate = rate;
-    }
+        ExchangeRate(double rate) {
+            this.rate = rate;
+        }
 
-    public double conversion (double beginningCurrencyAmount, ExchangeRate beginningCurrency, ExchangeRate endingCurrency){
-        double fromExchangeRate = beginningCurrency.getRate(); //USD
-        double toExchangeRate = endingCurrency.getRate(); //Euro
-        double conversionRate = toExchangeRate/fromExchangeRate; //Euro/USD
-        double convertedCurrency = conversionRate * beginningCurrencyAmount;
+        public static double conversion(double beginningCurrencyAmount, ExchangeRate beginningCurrency, ExchangeRate endingCurrency) {
+            double fromExchangeRate = beginningCurrency.getRate(); //USD
+            double toExchangeRate = endingCurrency.getRate(); //Euro
+            double conversionRate = toExchangeRate / fromExchangeRate; //Euro/USD
+            double convertedCurrency = conversionRate * beginningCurrencyAmount;
 
-        return convertedCurrency;
+            return convertedCurrency;
 
-    }
+        }
 
-
-
-
-}
+        }
--- src/test/java/com/zipcodewilmington/ExchangeRate/ExchangeRateTest.java
+++ src/test/java/com/zipcodewilmington/ExchangeRate/ExchangeRateTest.java
@@ -5,11 +5,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class ExchangeRateTest {
 
-//    private ExchangeRate testExchangeRate;
-    double currentCurrency = 100;
+    //    private ExchangeRate testExchangeRate;
+    double currentCurrency = 1;
+    public static final double delta = 0.01;
     ExchangeRate USD = ExchangeRate.USD;
     ExchangeRate GBP = ExchangeRate.GBP;
     ExchangeRate EUR = ExchangeRate.EUR;
@@ -22,19 +22,66 @@ public class ExchangeRateTest {
     ExchangeRate JPY = ExchangeRate.JPY;
     ExchangeRate CNY = ExchangeRate.CNY;
 
-//    @Before
-//    public void createExchangeRate(){
-//        this.testExchangeRate = new ExchangeRate();
-//    }
 
-@Test
-public void testUSDtoEuro(){
+    @Test
+    public void testUSDToEuro() {
 
-    double expected = 94.0;
-    double actual = ExchangeRateTest.conversion(currentCurrency, USD, EUR);
-    Assert.assertEquals(expected, actual, 0.1);
-}
+        double expected = .94;
+        double actual = ExchangeRate.conversion(currentCurrency, USD, EUR);
+        Assert.assertEquals(expected, actual, delta);
 
+    }
 
+    @Test
+    public void testEuroToUSD() {
 
+        double expected = 1.06;
+        double actual = ExchangeRate.conversion(currentCurrency, EUR, USD);
+        Assert.assertEquals(expected, actual, delta);
+
+    }
+
+    @Test
+    public void testEuroToGBP() {
+
+        double expected = .87;
+        double actual = ExchangeRate.conversion(currentCurrency, EUR, GBP);
+        Assert.assertEquals(expected, actual, delta);
+    }
+    @Test
+    public void testCNYToJPY() {
+
+        double expected = 16.73;
+        double actual = ExchangeRate.conversion(currentCurrency, CNY, JPY);
+        Assert.assertEquals(expected, actual, delta);
+    }
+    @Test
+    public void testJPYToCNY() {
+
+        double expected = 0.05;
+        double actual = ExchangeRate.conversion(currentCurrency, JPY, CNY);
+        Assert.assertEquals(expected, actual, delta);
+    }
+
+    @Test
+    public void testINRToCAD() {
+
+        double expected = .019;
+        double actual = ExchangeRate.conversion(currentCurrency, INR, CAD);
+        Assert.assertEquals(expected, actual, delta);
+    }
+    @Test
+    public void testAUDToSGD() {
+
+        double expected = 1.05;
+        double actual = ExchangeRate.conversion(currentCurrency, AUD, SGD);
+        Assert.assertEquals(expected, actual, delta);
+    }
+    @Test
+    public void testCHFToMYR() {
+
+        double expected = 4.42;
+        double actual = ExchangeRate.conversion(currentCurrency, CHF, MYR);
+        Assert.assertEquals(expected, actual, delta);
+    }
 }
