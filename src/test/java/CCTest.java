df360c928344f3106683b461501712d76f7f6593
ghost1497 <rfrankie04@gmail.com>
2018-02-15 17:50:47 -0500

    strings into tests

--- src/test/java/CCTest.java
+++ src/test/java/CCTest.java
@@ -8,106 +8,97 @@ import org.junit.Test;
 
 public class CCTest {
     private double delta = 0;
+    private final String USD = "us dollar";
+    private final String euro = "euro";
+    private final String bP = "british pound";
+    private final String iR = "indian rupee";
+    private final String a$ = "australian dollar";
+    private final String c$ = "canadian dollar";
+    private final String s$ = "singapore dollar";
+    private final String sF = "swiss franc";
+    private final String mR = "malaysian ringgit";
+    private final String jY = "japanese yen";
+    private final String cYR = "chinese yuan renminbi";
 
     @Test
     public void testFromUSDtoEuro(){
-        String start = "Us Dollar";
-        String end = "Euro";
         double inValue = 5.67;
         double expected = 5.33;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, USD, euro);
         Assert.assertEquals(expected, actual, delta);
     }
 
     @Test
     public void testFromEurotoUSD(){
-        String end = "Us Dollar";
-        String start = "Euro";
         double inValue = 150.87;
         double expected = 160.50;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, euro, USD);
         Assert.assertEquals(expected, actual, delta); }
 
     @Test
     public void testFromEurotoPound(){
-        String start = "EURO";
-        String end = "British Pound";
         double inValue = 1590.65;
         double expected = 1387.59;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, euro, bP);
         Assert.assertEquals(expected, actual, delta);    }
 
     @Test
     public void testFromPoundtoRupee(){
-        String end = "indian rupee";
-        String start = "British Pound";
         double inValue = 1590.65;
         double expected =132528.30;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, bP, iR);
         Assert.assertEquals(expected, actual, delta);    }
 
     @Test
     public void testFromRupeetoCanada(){
-        String start = "indian rupee";
-        String end = "canadian Dollar";
         double inValue = 76.00;
         double expected = 1.47;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, iR, c$);
         Assert.assertEquals(expected, actual, delta);    }
 
     @Test
     public void testFromCanadatoSingapore(){
-        String start = "Canadian Dollar";
-        String end = "singapore dollar";
         double inValue = 150.65;
         double expected = 163.20;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, c$, s$);
         Assert.assertEquals(expected, actual, delta);    }
 
     @Test
     public void testFromSingaporetoSwiss(){
-        String start = "Singapore Dollar";
-        String end = "Swiss Franc";
         double inValue = 1590.65;
         double expected = 1123.47;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, s$, sF);
         Assert.assertEquals(expected, actual, delta);    }
 
     @Test
     public void testFromSwisstoMalaysia(){
-        String start = "Swiss franc";
-        String end = "Malaysian Ringgit";
         double inValue = 1.99;
         double expected = 8.81;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, sF, mR);
         Assert.assertEquals(expected, actual, delta);    }
 
     @Test
     public void testFromMaltoJapan(){
-        String start = "Malaysian Ringgit";
-        String end = "Japanese Yen";
         double inValue = 5648.00;
         double expected = 146367.86;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, mR, jY);
         Assert.assertEquals(expected, actual, delta);    }
 
     @Test
     public void testFromJapantoChina(){
-        String end = "Chinese Yuan Renminbi";
-        String start = "Japanese Yen";
         double inValue = 785738;
         double expected = 46938.08;
         CurrencyConverter currencyConverter = new CurrencyConverter(inValue);
-        double actual = currencyConverter.convertingInterface(inValue, start, end);
+        double actual = currencyConverter.convertingInterface(inValue, jY, cYR);
         Assert.assertEquals(expected, actual, delta);
     }
 }
