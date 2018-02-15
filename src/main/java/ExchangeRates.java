70a0b82dbde7670eeba44791be52dca6c937f5c6
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-02-15 14:45:56 -0500

    first commit

---
+++ src/main/java/ExchangeRates.java
@@ -0,0 +1,41 @@
+import java.util.Locale;
+import java.util.Currency;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+
+
+
+public class ExchangeRates {
+
+    public ExchangeRates() {
+        double USDollar = (1.00);
+        double BritishPound = (0.94);
+        double IndianRupee = (68.32);
+        double AustralianDollar = (1.35);
+        double CanadianDollar = (1.32);
+        double SingaporeDollar = (1.43);
+        double SwissFranc = (1.01);
+        double MalaysianRinggit = (4.47);
+        double JapaneseYen = (115.84);
+        double ChineseYuanRenminbi = (6.92);
+    }
+
+    DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/TestExchangeRates.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import java.text.DecimalFormat;
+
+public class TestExchangeRates {
+
+
+
+
+    @Test
+
+
+}
