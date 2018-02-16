c3a6a5ed8dc0a3a02f90884889173d2e5442c56e
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-16 06:20:16 -0500

    the beginning of everything

---
+++ src/main/java/ConverterTest.java
@@ -0,0 +1,17 @@
+/*
+*
+* Created by amazing amy 2/16/18
+*
+* have a nice day
+*
+*
+ */
+
+public class ConverterTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    //last
+}
---
+++ src/main/java/CurrencyConverter.java
@@ -0,0 +1,20 @@
+/*
+*
+* Created by amazing amy 2/16/18
+*
+* have a nice day
+*
+*
+*
+ */
+
+public class CurrencyConverter {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+ //last
+}
